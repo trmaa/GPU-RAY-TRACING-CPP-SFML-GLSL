e7c21a440c4047452d12fa5d5a037ad2efa2fa8a
trmaa <trmaayt@gmail.com>
2024-12-26 11:19:05 +0100

    fixes

--- src/shaders/main.glsl
+++ src/shaders/main.glsl
@@ -11,7 +11,7 @@ uniform vec3 cam_dir;
 void main() {
     vec2 uv = (gl_FragCoord.xy / screen_size) * 2.0 - 1.0;
     uv.y = -uv.y;
-    uv.x = uv.x*screen_size.x/screen_size.y;
+    uv.x = uv.x * screen_size.x / screen_size.y;
 
     Ray ray = create_ray(cam_pos, cam_dir, uv);
 
@@ -23,27 +23,36 @@ void main() {
         Sphere(1.0, vec3(-1.5, -0.5, -4.5))
     );
 
-    float closest_t = -1.0;
-    vec3 closest_normal = ray.direction;
-    vec3 hit_point = ray.origin;
+    vec3 col = vec3(0.0);
+    vec3 attenuation = vec3(1.0);
+    float closest_t;
 
     for (int bounce = 0; bounce < 4; bounce++) {
+        closest_t = -1.0;
+        vec3 closest_normal = vec3(0.0);
+        vec3 hit_point = vec3(0.0);
+
         for (int i = 0; i < 5; ++i) {
             Sphere sphere = spheres[i];
             float t = check_collision(sphere, ray);
 
             if (t > 0.0 && (closest_t < 0.0 || t < closest_t)) {
                 closest_t = t;
-                vec3 hit_point = ray_at(ray, t);
+                hit_point = ray_at(ray, t);
                 closest_normal = normalize(sphere_normal(sphere, hit_point));
-            } else {
-                continue;
             }
         }
-        ray = cast_ray(hit_point + closest_normal*0.01, (closest_normal*2*dot(closest_normal, ray.direction))+ray.direction);
-    }
 
-    vec3 col = closest_t > 0.0 ? closest_normal * 0.5 + 0.5 : vec3(0.0);
+        if (closest_t < 0.0) {
+            break;
+        }
+
+        col += attenuation * (closest_normal * 0.5 + 0.5);
+        attenuation *= 0.5;
+
+        ray.origin = hit_point + closest_normal * 0.01;
+        ray.direction = reflect(ray.direction, closest_normal);
+    }
 
     gl_FragColor = vec4(col, 1.0);
 }
