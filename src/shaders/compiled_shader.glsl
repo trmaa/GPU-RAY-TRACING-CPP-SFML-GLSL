1b40014f7e420dc8838413e019876284df9efbee
trmaa <trmaayt@gmail.com>
2024-12-28 20:30:09 +0100

    some changes

--- src/shaders/compiled_shader.glsl
+++ src/shaders/compiled_shader.glsl
@@ -75,11 +75,11 @@ vec3 sphere_color(Sphere s) {
 }
 
 Sphere spheres[6] = Sphere[](
-    Sphere(0.1, vec3(0, 0, 0), vec3(1, 0.2, 1), 0, true),
-    Sphere(1.0, vec3(-2.0, 1.7, -5.0), vec3(1, 0.2, 0.2), 0.5, false),
-    Sphere(0.8, vec3(0.0, 1.0, -4.0), vec3(0.2, 0.2, 1), 0.5, false),
-    Sphere(1.2, vec3(2.0, -1.0, -6.0), vec3(0.2, 1, 0.2), 0.5, true),
-    Sphere(0.6, vec3(1.0, 1.0, -3.0), vec3(1, 1, 0.2), 0.5, false),
+    Sphere(0.1, vec3(0, 0, 0), vec3(1, 0.2, 1), 0, false),
+    Sphere(1.0, vec3(-2.0, 1.7, -5.0), vec3(1, 0.2, 0.2), 0.7, false),
+    Sphere(0.8, vec3(0.0, 1.0, -4.0), vec3(0.2, 0.2, 1), 0.7, false),
+    Sphere(1.2, vec3(2.0, -1.0, -6.0), vec3(0.2, 1, 0.2), 0.7, true),
+    Sphere(0.6, vec3(1.0, 1.0, -3.0), vec3(1, 1, 0.2), 0.7, false),
     Sphere(1.0, vec3(-1.5, -0.5, -4.5), vec3(0.2, 1, 1), 0, false)
 );
 
